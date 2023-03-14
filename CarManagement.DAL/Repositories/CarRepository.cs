463cc0c1ec1adbb6025a3b89b04bc4029ebc33c7
Andrey <Andrey@DESKTOP-NRKMLIG>
2023-03-14 23:51:18 +0300

    add usings

--- CarManagement.DAL/Repositories/CarRepository.cs
+++ CarManagement.DAL/Repositories/CarRepository.cs
@@ -8,48 +8,54 @@ namespace CarManagement.DAL.Repositories;
 
 public class CarRepository : ICarRepository
 {
-    private readonly IDbConnection _dbConnection;
+    private readonly string _connectionString;
 
     public CarRepository(string connectionString)
     {
-        _dbConnection = new SqlConnection(connectionString);
+        _connectionString = connectionString;
     }
 
     public async Task<IEnumerable<CarModel>> GetAllAsync()
     {
+        using IDbConnection dbConnection = new SqlConnection(_connectionString);
         const string sql = "SELECT * FROM Cars";
-        return await _dbConnection.QueryAsync<CarModel>(sql);
+        return await dbConnection.QueryAsync<CarModel>(sql);
     }
 
     public async Task<IEnumerable<CarModel>> GetSomeAsync(int skip, int take)
     {
+        using IDbConnection dbConnection = new SqlConnection(_connectionString);
         const string sql = "SELECT * FROM Cars ORDER BY Id OFFSET @Skip ROWS FETCH NEXT @Take ROWS ONLY";
-        return await _dbConnection.QueryAsync<CarModel>(sql, new { Skip = skip, Take = take });
+        return await dbConnection.QueryAsync<CarModel>(sql, new { Skip = skip, Take = take });
     }
 
     public async Task<CarModel> GetByIdAsync(int id)
     {
+        using IDbConnection dbConnection = new SqlConnection(_connectionString);
         const string sql = "SELECT * FROM Cars WHERE Id = @Id";
-        return await _dbConnection.QuerySingleOrDefaultAsync<CarModel>(sql, new { Id = id });
+        return await dbConnection.QuerySingleOrDefaultAsync<CarModel>(sql, new { Id = id });
     }
 
     public async Task<int> CreateAsync(CarCreateModel car)
     {
-        const string sql = "INSERT INTO Cars (Make, Model, Year, Color) VALUES (@Make, @Model, @Year, @Color); SELECT CAST(SCOPE_IDENTITY() as int)";
-        return await _dbConnection.ExecuteScalarAsync<int>(sql, car);
+        using IDbConnection dbConnection = new SqlConnection(_connectionString);
+        const string sql = "INSERT INTO Cars (Make, Model, Year, Color) VALUES (@Make, @Model, @Year, @Price); SELECT CAST(SCOPE_IDENTITY() as int)";
+        return await dbConnection.ExecuteScalarAsync<int>(sql, car);
     }
 
     public async Task<bool> UpdateAsync(CarUpdateModel car)
     {
-        const string sql = "UPDATE Cars SET Make = @Make, Model = @Model, Year = @Year, Color = @Color WHERE Id = @Id";
-        var affectedRows = await _dbConnection.ExecuteAsync(sql, car);
+        using IDbConnection dbConnection = new SqlConnection(_connectionString);
+        const string sql = "UPDATE Cars SET Make = @Make, Model = @Model, Year = @Year, Price = @Price WHERE Id = @Id";
+        var affectedRows = await dbConnection.ExecuteAsync(sql, car);
         return affectedRows > 0;
     }
 
     public async Task<bool> DeleteAsync(int id)
     {
+        using IDbConnection dbConnection = new SqlConnection(_connectionString);
         const string sql = "DELETE FROM Cars WHERE Id = @Id";
-        var affectedRows = await _dbConnection.ExecuteAsync(sql, new { Id = id });
+        var affectedRows = await dbConnection.ExecuteAsync(sql, new { Id = id });
         return affectedRows > 0;
     }
 }
