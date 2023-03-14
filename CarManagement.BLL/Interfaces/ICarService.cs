83f6541cb52ffc60f50973b9fff8bdd7b328c877
Andrey <61059883+kaallaash@users.noreply.github.com>
2023-03-15 01:40:00 +0300

    add bll layer (#6)
    Co-authored-by: Andrey <Andrey@DESKTOP-NRKMLIG>

---
+++ CarManagement.BLL/Interfaces/ICarService.cs
@@ -0,0 +1,13 @@
+﻿using CarManagement.Models.Car;
+
+namespace CarManagement.BLL.Interfaces;
+
+public interface ICarService
+{
+    Task<IEnumerable<CarModel>> GetAllAsync();
+    Task<IEnumerable<CarModel>> GetSomeAsync(int selectionEdge1, int selectionEdge2);
+    Task<CarModel> GetByIdAsync(int id);
+    Task<int> CreateAsync(CarCreateModel car);
+    Task<bool> UpdateAsync(CarUpdateModel car);
+    Task<bool> DeleteAsync(int id);
+}
---
+++ CarManagement.BLL/Services/UserService.cs
@@ -0,0 +1,37 @@
+﻿using CarManagement.BLL.Interfaces;
+using CarManagement.DAL.Interfaces;
+using CarManagement.Models;
+using CarManagement.Models.Authentication;
+
+namespace CarManagement.BLL.Services;
+
+public class UserService : IUserService
+{
+    private readonly IUserRepository _userRepository;
+
+    public UserService(
+        IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+
+    public async Task<UserModel> GetByLoginAsync(LoginModel loginModel)
+    {
+        return await _userRepository.GetByLoginAsync(loginModel);
+    }
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        return await _userRepository.DeleteAsync(id);
+    }
+
+    public async Task<int> RegisterAsync(RegisterModel registerModel)
+    {
+        return await _userRepository.RegisterAsync(registerModel);
+    }
+
+    public async Task<bool> UpdateAsync(RegisterModel registerModel)
+    {
+        return await _userRepository.UpdateAsync(registerModel);
+    }
+}
