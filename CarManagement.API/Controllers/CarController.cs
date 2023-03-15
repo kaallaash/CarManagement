f9a7f38bd3e7c1eed553043e96180bab0af2f0a8
Andrey <Andrey@DESKTOP-NRKMLIG>
2023-03-15 21:49:38 +0300

    add CarController

---
+++ CarManagement.API/Controllers/CarController.cs
@@ -0,0 +1,57 @@
+﻿using CarManagement.BLL.Interfaces;
+using CarManagement.Models.Car;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarManagement.API.Controllers;
+
+public class CarController : Controller
+{
+    private readonly ICarService _carService;
+
+    public CarController(
+        ICarService carService)
+    {
+        _carService = carService;
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<CarModel>> GetAll(
+    )
+    {
+        return await _carService.GetAllAsync();
+    }
+
+    [HttpGet]
+    public async Task<IEnumerable<CarModel>> GetSome(int selectionEdge1, int selectionEdge2)
+    {
+        return await _carService.GetSomeAsync(selectionEdge1, selectionEdge2);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<CarModel> GetById(int id)
+    {
+        return await _carService.GetByIdAsync(id);
+    }
+
+
+
+    [HttpPost]
+    public async Task<int> Create(
+        [FromBody] CarCreateModel carCreateModel)
+    {
+        return await _carService.CreateAsync(carCreateModel);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<bool> Update(int id,
+        [FromBody] CarUpdateModel carUpdateModel)
+    {
+        return await _carService.UpdateAsync(carUpdateModel);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<bool> Delete(int id)
+    {
+        return await _carService.DeleteAsync(id);
+    }
+}
